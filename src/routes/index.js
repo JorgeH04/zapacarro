fc4be06b49282575dee37aed2f5d67f1456f3dd0
Jorge <jhessle04@gmail.com>
2020-02-10 19:09:51 -0300

    H

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const Ofertauno = require('../models/ofertauno');
 const Ofertados = require('../models/ofertados');
 const Ofertatres = require('../models/ofertatres');
+const Cart = require('../models/cart');
 
 
 
@@ -15,7 +16,21 @@ router.get('/', async (req, res) => {
   });
 });
 
+router.get('/addtocardofertados/:id', function(req, res, next){
+  var productId = req.params.id;
+  var cart = new Cart(req.session.cart ? req.session.cart : {items: {}});
 
+  Ofertados.findById(productId, function(err, product){
+    if(err){
+      return res-redirect('/');
+    }
+    cart.add(product, product.id);
+    req.session.cart = cart;
+    console.log(req.session.cart);
+    res.redirect('/shopcart');
+
+  });
+});
 
 
 module.exports = router;
