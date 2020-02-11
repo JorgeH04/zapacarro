b0f2b4f75ba22f0db0c8b94d367c61f99b34615a
Jorge <jhessle04@gmail.com>
2020-02-11 00:45:07 -0300

    f

--- src/routes/produno.js
+++ src/routes/produno.js
@@ -8,7 +8,7 @@ const router = express.Router();
 // Models
 const Produno = require('../models/produno');
 const Cart = require('../models/cart');
-const Order = require('../models/Order');
+//const Order = require('../models/Order');
 
 // Helpers
 const { isAuthenticated } = require('../helpers/auth');
